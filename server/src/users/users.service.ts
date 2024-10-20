f9ad22b8d71d1da99421b5226c44ca71fe98cdd4
BOGI5 <bogdanyakov5@gmail.com>
2024-10-20 22:20:14 +0300

    .

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,21 +1,21 @@
-import { Injectable, ValidationPipe } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
+import { Injectable, ValidationPipe } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>,
+    private userRepository: Repository<User>
   ) {}
 
   async create(createUserDto: CreateUserDto) {
     const validationPipe = new ValidationPipe();
     await validationPipe.transform(createUserDto, {
-      type: 'body',
+      type: "body",
       metatype: CreateUserDto,
     });
     return this.userRepository.save(createUserDto);
@@ -36,7 +36,7 @@ export class UsersService {
   async update(id: number, updateUserDto: UpdateUserDto) {
     const validationPipe = new ValidationPipe();
     await validationPipe.transform(updateUserDto, {
-      type: 'body',
+      type: "body",
       metatype: UpdateUserDto,
     });
     return this.userRepository.update(id, updateUserDto);
