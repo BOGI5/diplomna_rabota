f71b8c602cc9bc1e0f5030f4d21cedf12d69b66c
BOGI5 <bogdanyakov5@gmail.com>
2024-10-23 14:26:31 +0300

    Added registration form

--- client/src/components/RegistrationForm.tsx
+++ client/src/components/RegistrationForm.tsx
@@ -6,6 +6,14 @@ export default function LoginForm() {
     <>
       <div className="p-inputgroup">
         <span className="p-inputgroup-addon">
+          <i className="pi pi-user"></i>
+        </span>
+        <InputText placeholder="First name" />
+        <InputText placeholder="Last name" />
+      </div>
+      <br />
+      <div className="p-inputgroup">
+        <span className="p-inputgroup-addon">
           <i className="pi pi-at"></i>
         </span>
         <InputText placeholder="Email" type="email" />
@@ -16,6 +24,7 @@ export default function LoginForm() {
           <i className="pi pi-lock"></i>
         </span>
         <Password placeholder="Password" feedback={false} />
+        <Password placeholder="Confirm password" feedback={false} />
       </div>
       <br />
     </>
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -2,16 +2,20 @@ import { Toast } from "primereact/toast";
 import { ButtonGroup } from "primereact/buttongroup";
 import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
+import { ToggleButton } from "primereact/togglebutton";
 import { useRef, useState } from "react";
 import LoginForm from "../../components/LoginForm";
+import RegistrationForm from "../../components/RegistrationForm";
 import environment from "../../environment";
 
 export default function Login() {
+  // TODO - add if state for width 500px
   const onGoogleLogin = () => {
     window.location.href = `${environment.apiUrl}/auth/google`;
   };
   const [visible, setVisible] = useState(true);
   const warning = useRef(null);
+  const [accountNotExists, setAccountNotExists] = useState(false);
   const showWarning = () => {
     warning.current.show({
       severity: "warn",
@@ -32,14 +36,30 @@ export default function Login() {
         header={"Login"}
       >
         <form action="" onSubmit={() => setVisible(false)}>
-          <LoginForm />
-          <br />
+          {accountNotExists ? <RegistrationForm /> : <LoginForm />}
+
+          <ToggleButton
+            offLabel="I already have an account"
+            onLabel="I don't have an account"
+            offIcon="pi pi-check"
+            onIcon="pi pi-times"
+            checked={accountNotExists}
+            onChange={(e) => setAccountNotExists(e.value)}
+          />
+          {!accountNotExists && (
+            <>
+              <br />
+              <br />
+            </>
+          )}
+
           <ButtonGroup>
             <Button label="Reset" type="reset" outlined />
             <Button label="Submit" type="submit" />
           </ButtonGroup>
         </form>
         <br />
+
         <Button
           icon="pi pi-google"
           iconPos="right"
