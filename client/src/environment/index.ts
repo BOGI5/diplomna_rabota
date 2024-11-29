b095d16773c4857e291f8153c2c4f558927c9302
BOGI5 <bogdanyakov5@gmail.com>
2024-11-29 15:00:04 +0200

    Fixed error messaging and added some endpoints to environment

--- client/src/environment/index.ts
+++ client/src/environment/index.ts
@@ -1,11 +1,18 @@
 const environment = {
   // API url
   apiUrl: "http://localhost:3000/api",
+  // Auth url
   oauthUrl: "/auth/google",
   signUpUrl: "/auth/sign-up",
   signInUrl: "/auth/sign-in",
   signOutUrl: "/auth/sign-out",
   refreshCredentialsUrl: "/auth/refresh",
+  //  User url
+  getAll: "/users",
+  getOne: "/users/:id",
+  getSelf: "/users/me",
+  updateSelf: "/users/me",
+  deleteSelf: "/users/me",
   // Client url
   clientUrl: "http://localhost:5173",
   clientAuthUrl: "/auth",
