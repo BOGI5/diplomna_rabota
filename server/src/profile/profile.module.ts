01270d199d7a87c76e5434ce50ddc1335b65f6b7
BOGI5 <bogdanyakov5@gmail.com>
2025-01-12 15:21:41 +0200

    Fixed imports/exports

--- server/src/profile/profile.module.ts
+++ server/src/profile/profile.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ProfileService } from "./profile.service";
 import { ProfileController } from "./profile.controller";
-import { AssignmentsModule } from "src/assignments/assignments.module";
-import { MembersModule } from "src/members/members.module";
 import { ProjectsModule } from "src/projects/projects.module";
+import { MembersModule } from "src/members/members.module";
+import { TasksModule } from "src/tasks/tasks.module";
 
 @Module({
-  imports: [AssignmentsModule, MembersModule, ProjectsModule],
+  imports: [TasksModule, MembersModule, ProjectsModule],
   controllers: [ProfileController],
   providers: [ProfileService],
   exports: [ProfileService],
