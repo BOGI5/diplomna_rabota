c46443d39261f245c937590decea7a84650ef3b1
BOGI5 <bogdanyakov5@gmail.com>
2025-01-12 14:41:04 +0200

    All local guards replaced with one global

--- server/src/assignments/assignments.controller.ts
+++ server/src/assignments/assignments.controller.ts
@@ -3,22 +3,20 @@ import { AssignmentsService } from "./assignments.service";
 import { AccessTokenGuard } from "src/auth/guards/accessToken.guard";
 
 @Controller("assignments")
+@UseGuards(AccessTokenGuard)
 export class AssignmentsController {
   constructor(private readonly assignmentsService: AssignmentsService) {}
 
-  @UseGuards(AccessTokenGuard)
   @Get()
   findAll() {
     return this.assignmentsService.findAll();
   }
 
-  @UseGuards(AccessTokenGuard)
   @Get(":id")
   findOne(@Param("id") id: string) {
     return this.assignmentsService.findOne(+id);
   }
 
-  @UseGuards(AccessTokenGuard)
   @Delete(":id")
   remove(@Param("id") id: string) {
     return this.assignmentsService.remove(+id);
